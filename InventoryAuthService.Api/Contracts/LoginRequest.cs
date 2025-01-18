9b2abdcdeeade53762e563ef133801b68f5a4462
Claury Mejia <clauryfranchesca.mejia@gmail.com>
2025-01-17 23:51:43 -0400

    first commit

---
+++ InventoryAuthService.Api/Contracts/LoginRequest.cs
@@ -0,0 +1,7 @@
+namespace InventoryAuthService.Api.Contracts;
+
+public record LoginRequest
+{
+    public string UserName { get; init; } = string.Empty;
+    public string Password { get; init; } = string.Empty;
+}
